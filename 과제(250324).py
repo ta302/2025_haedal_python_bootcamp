c59a00afdfbebb9179a73e346453e14a7b37551c
ta302 <ttyeong.e@gmail.com>
2025-03-26 18:36:11 +0900

    1차시-1,2,3

---
+++ 과제(250324).py
@@ -0,0 +1,19 @@
+width=40
+height=20
+area=width*height*0.5
+print("과제1")
+print(area)
+
+radius=11
+cir=3.142*2*radius
+area=3.142*radius*radius
+print("과제2")
+print(cir)
+print(area)
+
+print("과제3")
+n=int(input("정수 n을 입력하시오:"))
+if n%2==0:
+    print("True")
+else:
+    print("False")
